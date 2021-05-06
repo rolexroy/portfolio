910401ac8a078ef40b8cc5ecf0c02c29aeddacc6
rolexroy <rolexroney@gmail.com>
2021-05-06 13:28:32 +0300

    revised portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -22,7 +22,7 @@ export const aboutData = {
   paragraphOne: 'I am a versatile person who is ready to learn new ideas and explore opportunities beyond the bounds of my profession.',
   paragraphTwo: 'My ultimate goal is giving my clients a website they can be proud of, as well as a site that\'s easy for them to maintain in the future.',
   paragraphThree: 'I am a well organized person and a problem solver that pays attention to detail when involved in a project.',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://ecstatic-easley-f2b50b.netlify.app/', // if no resume, the button will not show up
 };
 
 // export const skills = [
@@ -66,7 +66,7 @@ export const projectsData = [
     info: 'One of the most unique & useful abilities of javascript is its ability to manipulate the DOM. In this simple RPS game, we understand how we can use javascript to make our webpage listen & react to events such as mouse-clicks & keypresses. ',
     info2: 'Technologies used : HTML5, CSS, Node.js',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://ecstatic-easley-f2b50b.netlify.app/', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -75,7 +75,7 @@ export const projectsData = [
     info: 'This is a simple shopping list app.You can use it for grocery shopping or any other listing needs that you may have.This app allows users to sign in to view, edit or remove an item from their customized list of items.',
     info2: 'Technologies used: HTML5, CSS , Node.js, Express, MySQL',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://ecstatic-easley-f2b50b.netlify.app/', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -84,7 +84,7 @@ export const projectsData = [
     info: 'Task manager is a simple app that allows you to plan and manage your tasks.This app is user friendly, you can easily add or remove crossed items from your list. This project demonstrates how we can use javascript to add new elements to the DOM or remove the existing ones.',
     info2: 'Technologies used: HTML5, CSS , Node.js, Express',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://ecstatic-easley-f2b50b.netlify.app/', // if no repo, the button will not show up
   },
 ];
 
@@ -101,7 +101,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://ecstatic-easley-f2b50b.netlify.app',
     },
     // {
     //   id: nanoid(),
@@ -111,12 +111,12 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://ecstatic-easley-f2b50b.netlify.app',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://ecstatic-easley-f2b50b.netlify.app/',
     },
   ],
 };
